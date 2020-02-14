f52ff28ac5ea6724740b9e06f05d4aaf31eb98e0
Armin <armin.babaei@gmail.com>
2020-02-13 19:05:49 -0500

    Code Formatting - added, configured and refactored prettier

--- services/client/src/components/AddUser.jsx
+++ services/client/src/components/AddUser.jsx
@@ -1,57 +1,53 @@
-import React from 'react';
-import propTypes from 'prop-types';
+import React from "react";
+import propTypes from "prop-types";
 
-const AddUser = (props) => {
-    return (
-        <form onSubmit={(event) => props.addUser(event)}>
-            <div className="field">
-                <label
-                  className="label is-large"
-                  htmlFor="input-username"
-                >Username
-                </label>
-                <input
-                  name="username"
-                  id="input-username"
-                  className="input is-large"
-                  type="text"
-                  placeholder="Enter a username"
-                  required
-                  value={props.username}
-                  onChange={props.handleChange}
-                />
-            </div>
-            <div className="field">
-                <label
-                  className="label is-large"
-                  htmlFor="input-email"
-                >Email
-                </label>
-                <input
-                  name="email"
-                  id="input-email"
-                  className="input is-large"
-                  type="email"
-                  placeholder="Enter an email address"
-                  required
-                  value={props.email}
-                  onChange={props.handleChange}
-                />
-            </div>
-            <input
-              type="submit"
-              className="button is-primary is-large is-fullwidth"
-              value="Submit"
-            />
-        </form>
-    )
+const AddUser = props => {
+  return (
+    <form onSubmit={event => props.addUser(event)}>
+      <div className="field">
+        <label className="label is-large" htmlFor="input-username">
+          Username
+        </label>
+        <input
+          name="username"
+          id="input-username"
+          className="input is-large"
+          type="text"
+          placeholder="Enter a username"
+          required
+          value={props.username}
+          onChange={props.handleChange}
+        />
+      </div>
+      <div className="field">
+        <label className="label is-large" htmlFor="input-email">
+          Email
+        </label>
+        <input
+          name="email"
+          id="input-email"
+          className="input is-large"
+          type="email"
+          placeholder="Enter an email address"
+          required
+          value={props.email}
+          onChange={props.handleChange}
+        />
+      </div>
+      <input
+        type="submit"
+        className="button is-primary is-large is-fullwidth"
+        value="Submit"
+      />
+    </form>
+  );
 };
 
 AddUser.propTypes = {
-    username: propTypes.string.isRequired,
-    email: propTypes.string.isRequired,
-    handleChange: propTypes.func.isRequired,
-    addUser: propTypes.func.isRequired,
-}
+  username: propTypes.string.isRequired,
+  email: propTypes.string.isRequired,
+  handleChange: propTypes.func.isRequired,
+  addUser: propTypes.func.isRequired
+};
 
 export default AddUser;
