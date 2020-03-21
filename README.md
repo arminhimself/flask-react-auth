df253c8f6650a0c57b92fc565a61767c00adba59
Armin Babaei <armin.babaei@gmail.com>
2020-03-20 23:58:25 -0400

    updating

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Authentication with Flask, React, and Docker
 
 [![pipeline status](https://gitlab.com/arminhimself/flask-react-auth/badges/master/pipeline.svg)](https://gitlab.com/arminhimself/flask-react-auth/commits/master)
+
+
+
+$ export REACT_APP_USERS_SERVICE_URL=http://localhost:5001
+$ docker-compose up -d --build
+
+$ docker-compose exec users python manage.py recreate_db
+$ docker-compose exec users python manage.py seed_db
+
+$ docker-compose exec users pytest "project/tests" -p no:warnings
+$ docker-compose exec users flake8 project
+$ docker-compose exec users black project
+$ docker-compose exec users /bin/sh -c "isort project/*/*.py"
--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
 import axios from "axios";
+import { Route, Switch } from "react-router-dom";
+import Modal from "react-modal";
 
 import UsersList from "./components/UsersList";
-import AddUser from "./components/AddUser";
 import About from "./components/About";
 import NavBar from "./components/NavBar";
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
+import UserStatus from "./components/UserStatus";
+import Message from "./components/Message";
+import AddUser from "./components/AddUser"; // new
+
+const modalStyles = {
+  content: {
+    top: "0",
+    left: "0",
+    right: "0",
+    bottom: "0",
+    border: 0,
+    background: "transparent"
+  }
+};
+
+Modal.setAppElement(document.getElementById("root"));
 
 class App extends Component {
   constructor() {
@@ -15,20 +31,19 @@ class App extends Component {
 
     this.state = {
       users: [],
-      username: "",
-      email: "",
-      title: "TestDriven.io"
+      title: "TestDriven.io",
+      accessToken: null,
+      messageType: null, // new
+      messageText: null, // new
+      showModal: false
     };
-
-    this.addUser = this.addUser.bind(this);
-    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
     this.getUsers();
   }
 
-  getUsers() {
+  getUsers = () => {
     axios
       .get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
       .then(res => {
@@ -37,46 +52,151 @@ class App extends Component {
       .catch(err => {
         console.log(err);
       });
-  }
+  };
 
-  addUser(event) {
-    event.preventDefault();
-    // console.log('sanity check!');
-    // console.log(this.state);
-
-    const data = {
-      username: this.state.username,
-      email: this.state.email
-    };
-
+  addUser = data => {
     axios
       .post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
       .then(res => {
-        // console.log(res);
         this.getUsers();
         this.setState({ username: "", email: "" });
+        this.handleCloseModal(); // new
+        this.createMessage("success", "User added.");
       })
       .catch(err => {
         console.log(err);
+        this.handleCloseModal(); // new
+        this.createMessage("danger", "That user already exists.");
       });
-  }
+  };
 
-  handleChange(event) {
-    const obj = {};
-    obj[event.target.name] = event.target.value;
-    this.setState(obj);
-  }
+  handleRegisterFormSubmit = data => {
+    const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/register`;
+    axios
+      .post(url, data)
+      .then(res => {
+        console.log(res.data);
+        // new
+        this.createMessage("success", "You have registered successfully.");
+      })
+      .catch(err => {
+        console.log(err);
+        // new
+        this.createMessage("danger", "That user already exists.");
+      });
+  };
+
+  handleLoginFormSubmit = data => {
+    const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/login`;
+    axios
+      .post(url, data)
+      .then(res => {
+        this.setState({ accessToken: res.data.access_token });
+        this.getUsers();
+        window.localStorage.setItem("refreshToken", res.data.refresh_token);
+        // new
+        this.createMessage("success", "You have logged in successfully.");
+      })
+      .catch(err => {
+        console.log(err);
+        // new
+        this.createMessage("danger", "Incorrect email and/or password.");
+      });
+  };
+
+  logoutUser = () => {
+    window.localStorage.removeItem("refreshToken");
+    this.setState({ accessToken: null });
+    // new
+    this.createMessage("success", "You have logged out.");
+  };
+
+  isAuthenticated = () => {
+    if (this.state.accessToken || this.validRefresh()) {
+      return true;
+    }
+    return false;
+  };
+
+  validRefresh = () => {
+    const token = window.localStorage.getItem("refreshToken");
+    if (token) {
+      axios
+        .post(`${process.env.REACT_APP_USERS_SERVICE_URL}/auth/refresh`, {
+          refresh_token: token
+        })
+        .then(res => {
+          this.setState({ accessToken: res.data.access_token });
+          this.getUsers();
+          window.localStorage.setItem("refreshToken", res.data.refresh_token);
+          return true;
+        })
+        .catch(err => {
+          return false;
+        });
+    }
+    return false;
+  };
+
+  createMessage = (type, text) => {
+    this.setState({
+      messageType: type,
+      messageText: text
+    });
+    // new
+    setTimeout(() => {
+      this.removeMessage();
+    }, 3000);
+  };
+
+  removeMessage = () => {
+    this.setState({
+      messageType: null,
+      messageText: null
+    });
+  };
+
+  handleOpenModal = () => {
+    this.setState({ showModal: true });
+  };
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false });
+  };
+
+  removeUser = user_id => {
+    axios
+      .delete(`${process.env.REACT_APP_USERS_SERVICE_URL}/users/${user_id}`)
+      .then(res => {
+        this.getUsers();
+        this.createMessage("success", "User removed.");
+      })
+      .catch(err => {
+        console.log(err);
+        this.createMessage("danger", "Something went wrong.");
+      });
+  };
 
   render() {
     return (
       <div>
-        <NavBar title={this.state.title} />
+        <NavBar
+          title={this.state.title}
+          logoutUser={this.logoutUser}
+          isAuthenticated={this.isAuthenticated} // new
+        />
         <section className="section">
           <div className="container">
+            {this.state.messageType && this.state.messageText && (
+              <Message
+                messageType={this.state.messageType}
+                messageText={this.state.messageText}
+                removeMessage={this.removeMessage} // new
+              />
+            )}
             <div className="columns">
               <div className="column is-half">
                 <br />
-                {/* new */}
                 <Switch>
                   <Route
                     exact
@@ -86,22 +206,76 @@ class App extends Component {
                         <h1 className="title is-1">Users</h1>
                         <hr />
                         <br />
-                        <AddUser
-                          username={this.state.username}
-                          email={this.state.email}
-                          addUser={this.addUser}
-                          // eslint-disable-next-line react/jsx-handler-names
-                          handleChange={this.handleChange}
+                        {this.isAuthenticated() && (
+                          <button
+                            onClick={this.handleOpenModal}
+                            className="button is-primary"
+                          >
+                            Add User
+                          </button>
+                        )}
+                        <br />
+                        <br />
+                        <Modal
+                          isOpen={this.state.showModal}
+                          style={modalStyles}
+                        >
+                          <div className="modal is-active">
+                            <div className="modal-background" />
+                            <div className="modal-card">
+                              <header className="modal-card-head">
+                                <p className="modal-card-title">Add User</p>
+                                <button
+                                  className="delete"
+                                  aria-label="close"
+                                  onClick={this.handleCloseModal}
+                                />
+                              </header>
+                              <section className="modal-card-body">
+                                <AddUser addUser={this.addUser} />
+                              </section>
+                            </div>
+                          </div>
+                        </Modal>
+                        <UsersList
+                          users={this.state.users}
+                          removeUser={this.removeUser}
+                          isAuthenticated={this.isAuthenticated}
                         />
-                        <br />
-                        <br />
-                        <UsersList users={this.state.users} />
                       </div>
                     )}
                   />
                   <Route exact path="/about" component={About} />
-                  <Route exact path="/register" component={RegisterForm} />
-                  <Route exact path="/login" component={LoginForm} />
+                  <Route
+                    exact
+                    path="/register"
+                    render={() => (
+                      <RegisterForm
+                        handleRegisterFormSubmit={this.handleRegisterFormSubmit}
+                        isAuthenticated={this.isAuthenticated}
+                      />
+                    )}
+                  />
+                  <Route
+                    exact
+                    path="/login"
+                    render={() => (
+                      <LoginForm
+                        handleLoginFormSubmit={this.handleLoginFormSubmit}
+                        isAuthenticated={this.isAuthenticated}
+                      />
+                    )}
+                  />
+                  <Route
+                    exact
+                    path="/status"
+                    render={() => (
+                      <UserStatus
+                        accessToken={this.state.accessToken}
+                        isAuthenticated={this.isAuthenticated}
+                      />
+                    )}
+                  />
                 </Switch>
               </div>
             </div>
